491a2545d9c3a35acefaf3ce737aff7f9feee07c
Patrick <patrick@sendhelp.biz>
2020-09-17 12:40:49 -0400

    to-do-events-part-II Completed

--- app.js
+++ app.js
@@ -1,52 +1,44 @@
 function onReady() {
+	let toDos = [];
 	const addToDoForm = document.getElementById('addToDoForm');
 
-	const newToDoText = document.getElementById('newToDoText');
-
-	const toDoList = document.getElementById('toDoList');
-	
-	addToDoForm.addEventListener('submit', () => {
-		event.preventDefault();
-
-		
-
-		// get the text
-		let title = newToDoText.value;
-
-		// create a new li
-		let newLi = document.createElement('li');
-
-		// create a new input
-		let checkbox = document.createElement('input');
-
-		// set the input's type to checkbox
-		checkbox.type = "checkbox";
-
-		let deleteBtn = document.createElement('button'); 
-		deleteBtn.textContent = "Delete";
-
-		deleteBtn.addEventListener('click', function(event) {
-			//this.parentElement 
-			toDoList.removeChild(this.parentElement);
-
-		})
-
-		// set the title
-		newLi.textContent = title;
-
-		// attach the checkbox to the li
-		newLi.appendChild(checkbox);
-
-		newLi.appendChild(deleteBtn);
-
-		// attach the li to the ul
-		toDoList.appendChild(newLi);
-
-		//empty the input
+	function createNewToDo() {
+		const newToDoText = document.getElementById('newToDoText');
+		if (!newToDoText.value) { return; }
+		toDos.push({
+			title: newToDoText.value,
+			complete: false
+		});
 		newToDoText.value = '';
 
+		renderTheUI();
+	}
+
+	function renderTheUI() {
+		const toDoList = document.getElementById('toDoList');
+
+		toDoList.textContent = '';
+
+		toDos.forEach(function(toDo){
+			const newLi = document.createElement('li');
+			const checkbox = document.createElement('input');
+			checkbox.type = "checkbox";
+
+			newLi.textContent = toDo.title;
+
+			toDoList.appendChild(newLi);
+			newLi.appendChild(checkbox);
+
+		});
+	}
+
+	addToDoForm.addEventListener('submit', event => {
+		event.preventDefault();
+		createNewToDo();
 	});
-};
+
+	renderTheUI();
+}
 
 window.onload = function() {
 	onReady();
